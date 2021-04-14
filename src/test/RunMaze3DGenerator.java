88813b6d90fd684ee0fd4be923c3e2b4054bf64c
ayemoy <ayelet431@gmail.com>
2021-04-14 16:18:11 +0300

    add tests

--- src/test/RunMaze3DGenerator.java
+++ src/test/RunMaze3DGenerator.java
@@ -2,25 +2,27 @@ package test;
 import algorithms.maze3D.IMaze3DGenerator;
 import algorithms.maze3D.Maze3D;
 import algorithms.maze3D.MyMaze3DGenerator;
-import algorithms.maze3D.SearchableMaze3D;
-import algorithms.mazeGenerators.*;
-import algorithms.search.*;
-import java.util.ArrayList;
+import algorithms.maze3D.Position3D;
+
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class RunMaze3DGenerator {
     public static void main(String[] args) {
-        IMaze3DGenerator lolo = new MyMaze3DGenerator();
-        Maze3D maze = lolo.generate(20, 50,40);
-        //SearchableMaze3D searchableMaze = new SearchableMaze3D(maze);
-
-        //long time = lolo.measureAlgorithmTimeMillis(9, 19, 19);
-       // System.out.println(time/1000);
+        testMazeGenerator(new MyMaze3DGenerator());
+    }
+    private static void testMazeGenerator(IMaze3DGenerator mazeGenerator) {
+// prints the time it takes the algorithm to run
+        System.out.println(String.format("Maze generationtime(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(100/*depth*/,100/*rows*/,100/*columns*/)));
+// generate another maze
+        Maze3D maze = mazeGenerator.generate(2/*depth*/,7/*rows*/, 7/*columns*/);
+// prints the maze
         maze.print();
-
-        //searchableMaze.getAllSuccessors();
-
-  }
-
+// get the maze entrance
+        Position3D startPosition = maze.getStartPosition();
+// print the start position
+        System.out.println(String.format("Start Position: %s", startPosition)); // format "{row,column}"
+// prints the maze exit position
+        System.out.println(String.format("Goal Position: %s", maze.getGoalPosition()));
+    }
 }
